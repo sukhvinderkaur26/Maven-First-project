a3bc5f96e7be962db7e2e4ca19bf423358a1242c
sukhvinderkaur26 <sukhvinder2630@gmail.com>
2022-03-24 09:57:47 -0600

    First maven project

---
+++ src/main/java/Hello.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        //Collection collection=new ArrayList();
+        Collection<String > collection=new ArrayList();
+        //collection.add(2);
+        collection.add("Sukhi");
+        for (String s:collection)
+        {
+            System.out.println(s.toUpperCase(Locale.ROOT));
+        }
+        List<String> list=new ArrayList<>();
+        list.add("sukhi");
+        list.add("kaur");
+    }
+}
